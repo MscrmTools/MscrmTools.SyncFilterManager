dcddd8cb5e5e97535cab2018eda35d132aa2aa4c
Tanguy TOUZARD <tanguy92@hotmail.com>
2017-03-22 17:45:48 +0100

    Initial commit

---
+++ MscrmTools.SyncFilterManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sync Filter Manager")]
+[assembly: AssemblyDescription("Manage filters for offline, Outlook and server side synchronizations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MscrmTools")]
+[assembly: AssemblyProduct("MscrmTools.SyncFilterManager")]
+[assembly: AssemblyCopyright("Copyright © MscrmTools 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8234074-9ff2-4a4c-b580-a2ad4507a116")]
+[assembly: AssemblyVersion("1.2017.3.1")]
+[assembly: AssemblyFileVersion("1.2017.3.1")]
